76cb15c5f4ec6e996b42e0620a03780c76203272
rdavini-hominiss <rafael.davini@hominiss.com.br>
2022-10-06 10:57:20 -0300

    [ST-03] create itemtypefreetaxes table, add notnull constraint to item field and calculateTax logic

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,25 @@
 class Item < ApplicationRecord
-    
+    attribute :total_price, :float, default: 0.0
+    after_initialize :calculateTax
+
+    def calculateTax
+        self.total_price = self.price + basic_tax + required_tax
+    end
+
+    def required_tax
+        price*5/100
+    end
+
+    def basic_tax
+        is_free_basic_tax ? 0 : price/10
+    end
+
+    def is_free_basic_tax
+        item_type_free_tax = ItemTypeFreeTax.all
+
+        item_type_free_tax.each do |it|
+            return true if self.desc.include? it.name
+        end
+        return false
+    end
 end
---
+++ app/models/item_type_free_tax.rb
@@ -0,0 +1,2 @@
+class ItemTypeFreeTax < ApplicationRecord
+end
